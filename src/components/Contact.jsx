66c0967fc5537e5921df420fb6eaad9ab221882d
Bruno Sousa <brunolpsousa@gmail.com>
2023-03-22 15:21:29 -0300

    Add files

---
+++ src/components/Contact.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import Title from './Title'
+import { gfToken } from '../data/getform'
+
+function Contact() {
+  return (
+    <div className='flex flex-col mb-10 mx-auto'>
+      <div className='flex justify-center items-center'>
+        <form
+          action={gfToken}
+          method='POST'
+          className='flex flex-col w-full md:w-7/12'
+        >
+          <Title>Contato</Title>
+          <input
+            type='hidden'
+            name='_gotcha'
+            className='hidden !imporant'
+          />
+          <input
+            type='text'
+            name='Name'
+            placeholder='Nome'
+            className='p-2 bg-transparent border-2 dark:border-stone-500 rounded-md focus:outline-none'
+          />
+          <input
+            type='text'
+            name='Email'
+            placeholder='Email'
+            className='my-2 p-2 bg-transparent border-2 dark:border-stone-500 rounded-md focus:outline-none'
+          />
+          <textarea
+            name='Message'
+            placeholder='Messagem'
+            rows='10'
+            className='p-2 mb-4 bg-transparent border-2 dark:border-stone-500 rounded-md focus:outline-none'
+          />
+          <button
+            type='submit'
+            className='text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white dark:text-stone-900 bg-gradient-to-r from-green-500 to-green-300 drop-shadow-md hover:stroke-gray-100'
+          >
+            Enviar
+          </button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/components/Intro.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Intro() {
+  return (
+    <div className='flex items-center justify-center flex-col text-center pt-20 pb-6'>
+      <h1 className='text-4xl md:text-7xl text-stone-800 dark:text-stone-50 mb-1 md:mb-3 font-bold'>
+        Bruno Sousa
+      </h1>
+      <p className='text-base md:text-xl text-stone-600 dark:text-stone-100 mb-3 font-medium'>
+        Software Developer
+      </p>
+      <p className='text-sm max-w-xl mb-6 text-stone-500 dark:text-stone-300 font-semibold'>
+        Programador e estudante de Análise e Desenvolvimento de Sistemas.
+        <br /> Em busca de novos desafios e aprendizado contínuo.
+      </p>
+    </div>
+  )
+}
+
+export default Intro
