d5ffab318bd1fd6bad50e01d41488e91ed08a414
Bruno Sousa <brunolpsousa@gmail.com>
2023-03-23 23:18:32 -0300

    Deploy

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,9 +1,11 @@
 export default function ErrorPage() {
   return (
-    <div className='flex items-center justify-center flex-col text-center pt-20 my-56 pb-6'>
-      <p className='text-base md:text-4xl text-stone-600 dark:text-stone-100 mb-3 font-medium'>
-        Desculpe, não foi possível carregar esta página
-      </p>
+    <div className='pt-40'>
+      <div className='flex items-center justify-center flex-col text-center my-56 pb-6'>
+        <p className='text-base md:text-4xl text-stone-600 dark:text-stone-100 mb-3 font-medium'>
+          Desculpe, não foi possível carregar esta página
+        </p>
+      </div>
     </div>
   )
 }
--- src/components/ThankYou.jsx
+++ src/components/ThankYou.jsx
@@ -1,12 +1,14 @@
 export default function ThankYou() {
   return (
-    <div className='flex items-center justify-center flex-col text-center pt-20 my-56 pb-6'>
-      <p className='text-base md:text-4xl text-stone-600 dark:text-stone-100 mb-3 font-medium'>
-        Mensagem enviada com sucesso
-      </p>
-      <p className='text-base md:text-xl max-w-xl mb-6 text-stone-500 dark:text-stone-300 font-semibold'>
-        Obrigado!
-      </p>
+    <div className='pt-40'>
+      <div className='flex items-center justify-center flex-col text-center pt-20 my-56 pb-6'>
+        <p className='text-base md:text-4xl text-stone-600 dark:text-stone-100 mb-3 font-medium'>
+          Mensagem enviada com sucesso
+        </p>
+        <p className='text-base md:text-xl max-w-xl mb-6 text-stone-500 dark:text-stone-300 font-semibold'>
+          Obrigado!
+        </p>
+      </div>
     </div>
   )
 }
