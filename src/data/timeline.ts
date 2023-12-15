874f661034cbcaf225e12c55df583dea62a1aaaa
Bruno Sousa <brunolpsousa@gmail.com>
2023-12-14 21:47:29 -0300

    feat: update timeline data

--- src/data/timeline.ts
+++ src/data/timeline.ts
@@ -1,9 +1,9 @@
 export default {
   pt: [
     {
-      year: 'Hoje',
+      year: '2023',
       title: 'Back-end Journey (Node.js) - AWS Cloud Context',
-      duration: ['2023/7/3'],
+      duration: ['2023/7/3', '2023/11/29'],
       details:
         'Surgiu uma oportunidade muito bacana de estágio na Compass.UOL. O objetivo é aprofundar os conhecimentos em AWS e Node.js; JavaScript, TypeScript, Express, MongoDB e Mongoose; criação de APIs, autenticação, e SSR; AWS Well-Architected Framework; método ágil Scrum e boas práticas de versionamento. Será uma jornada incrível!',
     },
@@ -17,9 +17,9 @@ export default {
   ],
   en: [
     {
-      year: 'Today',
+      year: '2023',
       title: 'Back-end Journey (Node.js) - AWS Cloud Context',
-      duration: ['2023/7/3'],
+      duration: ['2023/7/3', '2023/11/29'],
       details:
         'A very nice internship opportunity at Compass.UOL came up. The objective is to deepen knowledge in AWS and Node.js; JavaScript, TypeScript, Express, MongoDB and Mongoose; creating APIs, authentication, and SSR; AWS Well-Architected Framework; Agile Scrum methodology and good versioning practices. It will be an incredible journey!',
     },
